7ded19182293a9734e9ec0d67f5c9f78da87c1e4
Fernando Fagundes de  Andrade <fernandofagunde@gmail.com>
2026-04-30 13:32:39 -0300

    Projeto inicial Selenium Grid com JUnit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,80 @@
+
+import java.net.URL;
+import java.time.Duration;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariOptions;
+
+public class LoginTest {
+
+    @Test
+    public void loginChrome() throws Exception {
+
+        ChromeOptions options = new ChromeOptions();
+
+        WebDriver driver = new RemoteWebDriver(
+                new URL("http://localhost:4444"),
+                options);
+
+        executarTeste(driver);
+    }
+
+    @Test
+    public void loginFirefox() throws Exception {
+
+        FirefoxOptions options = new FirefoxOptions();
+
+        WebDriver driver = new RemoteWebDriver(
+                new URL("http://localhost:4444"),
+                options);
+
+        executarTeste(driver);
+    }
+
+    @Test
+    public void loginSafari() throws Exception {
+
+        SafariOptions options = new SafariOptions();
+
+        WebDriver driver = new RemoteWebDriver(
+                new URL("http://localhost:4444"),
+                options);
+
+        executarTeste(driver);
+    }
+
+    public void executarTeste(WebDriver driver) throws Exception {
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+
+        WebElement username =
+                driver.findElement(By.id("username"));
+
+        WebElement password =
+                driver.findElement(By.id("password"));
+
+        WebElement submit =
+                driver.findElement(By.id("submit"));
+
+        username.sendKeys("student");
+
+        password.sendKeys("Password123");
+
+        submit.click();
+
+        System.out.println("Título da página: "
+                + driver.getTitle());
+
+        Thread.sleep(3000);
+
+        driver.quit();
+    }
+}
